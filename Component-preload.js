686fd566841d21cc37c99a2643cdd9e7fd5db3ae
meritong <gashi.toni@gmail.com>
2024-09-05 22:53:56 +0200

    Add files via upload

---
+++ Component-preload.js
@@ -0,0 +1,20 @@
+//@ui5-bundle project1/Component-preload.js
+sap.ui.require.preload({
+	"project1/Component.js":function(){
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","project1/model/models"],function(e,t,i){"use strict";return e.extend("project1.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(i.createDeviceModel(),"device")}})});
+},
+	"project1/controller/App.controller.js":function(){
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("project1.controller.App",{onInit:function(){}})});
+},
+	"project1/controller/View1.controller.js":function(){
+sap.ui.define(["sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel","sap/m/MessageToast"],function(e,o,s){"use strict";return e.extend("project1.controller.View1",{onInit:function(){var e=new o;this.getView().setModel(e,"schedule")},onFetchSchedule:function(){var e=this.getView().getModel("schedule");$.ajax({url:"/scheduleApi/api/schedule?bookableAssetIds=c641abdd-6352-477b-8d34-d5b299922330&fromInclusive=2024-09-30&toInclusive=2024-10-06",method:"GET",success:function(e){console.log(e)},error:function(e){console.log("Error:",e)}})}})});
+},
+	"project1/i18n/i18n.properties":'# This is the resource bundle for project1\n\n#Texts for manifest.json\n\n#XTIT: Application name\nappTitle=App Title\n\n#YDES: Application description\nappDescription=An SAP Fiori application.\n#XTIT: Main view title\ntitle=App Title',
+	"project1/manifest.json":'{"_version":"1.65.0","sap.app":{"id":"project1","type":"application","i18n":"i18n/i18n.properties","applicationVersion":{"version":"0.0.1"},"title":"{{appTitle}}","description":"{{appDescription}}","resources":"resources.json","sourceTemplate":{"id":"@sap/generator-fiori:basic","version":"1.14.5","toolsId":"0e2f3b6f-f91b-46c2-9959-327cd5150d19"}},"sap.ui":{"technology":"UI5","icons":{"icon":"","favIcon":"","phone":"","phone@2":"","tablet":"","tablet@2":""},"deviceTypes":{"desktop":true,"tablet":true,"phone":true}},"sap.ui5":{"flexEnabled":true,"dependencies":{"minUI5Version":"1.127.1","libs":{"sap.m":{},"sap.ui.core":{},"sap.f":{},"sap.suite.ui.generic.template":{},"sap.ui.comp":{},"sap.ui.generic.app":{},"sap.ui.table":{},"sap.ushell":{}}},"contentDensities":{"compact":true,"cozy":true},"models":{"i18n":{"type":"sap.ui.model.resource.ResourceModel","settings":{"bundleName":"project1.i18n.i18n"}}},"resources":{"css":[{"uri":"css/style.css"}]},"routing":{"config":{"routerClass":"sap.m.routing.Router","viewType":"XML","async":true,"viewPath":"project1.view","controlAggregation":"pages","controlId":"app","clearControlAggregation":false},"routes":[{"name":"RouteView1","pattern":":?query:","target":["TargetView1"]}],"targets":{"TargetView1":{"viewType":"XML","transition":"slide","clearControlAggregation":false,"viewId":"View1","viewName":"View1"}}},"rootView":{"viewName":"project1.view.App","type":"XML","async":true,"id":"App"}}}',
+	"project1/model/models.js":function(){
+sap.ui.define(["sap/ui/model/json/JSONModel","sap/ui/Device"],function(e,n){"use strict";return{createDeviceModel:function(){var i=new e(n);i.setDefaultBindingMode("OneWay");return i}}});
+},
+	"project1/view/App.view.xml":'<mvc:View controllerName="project1.controller.App"\n    xmlns:html="http://www.w3.org/1999/xhtml"\n    xmlns:mvc="sap.ui.core.mvc" displayBlock="true"\n    xmlns="sap.m"><App id="app"></App></mvc:View>\n',
+	"project1/view/View1.view.xml":'<mvc:View controllerName="project1.controller.View1"\n    xmlns:mvc="sap.ui.core.mvc" displayBlock="true"\n    xmlns="sap.m"><Page title="Booking Schedule"><content><Button\n                text="Fetch Schedule"\n                press="onFetchSchedule" /><List\n                id="scheduleList"\n                headerText="Schedule"><items><ObjectListItem\n                        title="{date}"\n                        number="{time}"\n                        numberUnit="{status}" /></items></List></content></Page></mvc:View>\n'
+});
+//# sourceMappingURL=Component-preload.js.map
---
+++ controller/View1-dbg.controller.js
@@ -0,0 +1,42 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, MessageToast) {
+    "use strict";
+
+    return Controller.extend("project1.controller.View1", {
+        onInit: function () {
+            // Initialize the JSONModel
+            var oModel = new JSONModel();
+            this.getView().setModel(oModel, "schedule");
+        },
+
+        onFetchSchedule: function () {
+            var oModel = this.getView().getModel("schedule");
+
+           // var sUrl = "/scheduleApi/schedule?bookableAssetIds=c641abdd-6352-477b-8d34-d5b299922330&fromInclusive=2024-09-02&toInclusive=2024-09-08";
+
+
+            $.ajax({
+                url: "/scheduleApi/api/schedule?bookableAssetIds=c641abdd-6352-477b-8d34-d5b299922330&fromInclusive=2024-09-30&toInclusive=2024-10-06",
+                method: "GET",
+                success: function(response) {
+                  console.log(response);
+                },
+                error: function(error) {
+                  console.log("Error:", error);
+                }
+              });
+            /* oModel.loadData(sUrl);
+
+            oModel.attachRequestCompleted(function() {
+                MessageToast.show("Schedule fetched successfully!");
+            });
+
+            oModel.attachRequestFailed(function() {
+                MessageToast.show("Failed to fetch schedule!");
+            }); */
+        }
+    });
+});
